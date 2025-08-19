27048ba45300f39fdf7a52128c0f8c2cbf29a4ef
Eduardo Frei Bruno Gómez <eduardofreib@gmail.com>
2025-08-18 22:18:30 -0500

    fix: corregida importación de Firebase usando alias '@/firebase'

--- src/pages/oficina/OficinaSlug.jsx
+++ src/pages/oficina/OficinaSlug.jsx
@@ -1,7 +1,6 @@
 import { useParams } from "react-router-dom"; // O useRouter() en Next.js
 import { useEffect, useState } from "react";
-import { db } from "../../firebase";
-import { doc, getDoc, collection, addDoc } from "firebase/firestore";
+import { doc, getDoc, collection, addDoc } from "@/firebase";
 
 const OficinaPublica = () => {
   const { slug } = useParams();
