48eeddbad26ed3b77d2c17ad769f753c0589c1a4
Eduardo Frei Bruno Gómez <eduardofreib@gmail.com>
2025-08-22 20:28:53 -0500

    Elimina temporalmente OficinaSlug.jsx por conflictos con importación

--- src/pages/oficina/OficinaSlug.jsx
+++ src/pages/oficina/OficinaSlug.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom"; // O useRouter() en Next.js
 import { useEffect, useState } from "react";
-import { db, storage } from "@/firebase";
+import { db } from "@/firebase";
 import { doc, getDoc, collection, addDoc } from "firebase/firestore";
 
 const OficinaPublica = () => {
