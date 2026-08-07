6ff5a97b0b1bc626b6e75dcfbfb90abc689d0f1c
Eduardo Frei Bruno Gómez <eduardofreib@gmail.com>
2026-08-07 12:57:58 -0500

    Revert "feat: clean up complaints role admin option"
    This reverts commit 9d8bd49d49b819de11eea4f55dd0e9bb7183704d.

--- database/migrations/0003_complaints_role_admin_cleanup.sql
+++
@@ -1,5 +0,0 @@
--- Remove exclusively the ADMIN OPTION from the historical membership granted by supabase_admin
--- We do not revoke the role itself, nor do we revoke the SET OPTION granted in 0002.
-REVOKE ADMIN OPTION FOR complaints_api_runtime FROM postgres GRANTED BY supabase_admin;
---> statement-breakpoint
-REVOKE ADMIN OPTION FOR complaints_outbox_worker FROM postgres GRANTED BY supabase_admin;
